aaa4866709585139130dce041d9b0df0e11ae192
Pouya Asgharnejad Tehran <pouyaaofficial@gmail.com>
2022-07-09 17:37:10 +0430

    Update tests

---
+++ tests/Integration/SampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Integration;
+
+use Core\Testing\TestCase;
+
+class SampleTest extends TestCase
+{
+	public function test_it_can_run_integration_test()
+	{
+		$this->assertTrue(true);
+	}
+}
---
+++ tests/Unit/SampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use Core\Testing\TestCase;
+
+class SampleTest extends TestCase
+{
+	public function test_it_can_run_unit_test()
+	{
+		$this->assertTrue(true);
+	}
+}
